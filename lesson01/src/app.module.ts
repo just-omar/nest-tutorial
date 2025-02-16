5787fa86dd33a932d2533a1a769849c2c9f4be39
SchoolBoy98 <medzhidov.omar11@gmail.com>
2025-02-16 17:08:38 +0300

    minor

--- lesson01/src/app.module.ts
+++ lesson01/src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { BrusersController } from './brusers/brusers.controller';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController, BrusersController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
